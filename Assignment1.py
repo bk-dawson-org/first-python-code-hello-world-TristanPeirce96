fe70ffcb26faeba9a8a9163fcba56ad8c54c7140
TristanPeirce96 <tristan.peirce@dawsoncollege.qc.ca>
2024-08-30 23:36:02 +0000

    First Assignment, class 2, August 30, 2024

---
+++ Assignment1.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+myage = 20
+print("This is my age:")
+print(myage)
+yourage = input("What is your age? ")
+firstname = input("Entre your first name, please? ")
+lastname = input("And your last name? ")
+initial = input("And your middle initial(s)? ")
+print("Your full name is",firstname,initial,lastname,"and you are",yourage,"years old.")
